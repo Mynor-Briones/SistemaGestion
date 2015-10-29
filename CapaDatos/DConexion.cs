ed56bca6a86df5b3c3b87cc52602000c25fe0a99
Lucas Toneatto <lucastoneatto@gmail.com>
2015-10-29 20:31:51 -0300

    Clase Conexion
    Clase para conectarse a la BD

---
+++ CapaDatos/DConexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DConexion
+    {
+        public static String CnBDEmpresa = @"Data Source=LUCAS\SQLEXPRESS;Initial Catalog=BD_Gestion;Integrated Security=True";
+        public static String CnMaster = @"Data Source=LUCAS\SQLEXPRESS;Initial Catalog=BD_Gestion;Integrated Security=True";
+
+        public String ChequearConexion()
+        {
+            String mensaje = "";
+            SqlConnection SqlConexion = new SqlConnection();
+
+            try
+            {
+                SqlConexion.ConnectionString = DConexion.CnBDEmpresa;
+                SqlConexion.Open();
+                mensaje = "Y";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            finally
+            {
+                SqlConexion.Close();
+            }
+
+            return mensaje;
+        }
+    }
+}
